bafd9983e2ef028b34d64b7c1969ce729a23f68f
jbjordanleemac <56491177+jbjordanleemac@users.noreply.github.com>
2019-10-12 15:21:35 -0700

    Create python_10121901.py

---
+++ python_10121901.py
@@ -0,0 +1,10 @@
+class family:
+  love_god = 'yes'
+  love_family = 'yes'
+  
+  def __init__(self, fname, lname):
+    self.fname = fname
+    self.lname = lname
+    
+f1 =family('Mike', 'Burt')
+print(f1.fname)
